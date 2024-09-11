0e57203e2dbaa3cf54e9c2e63be4214dbe8f27c6
HactuallyBenji <mrbenamiller@gmail.com>
2024-09-11 21:01:47 +0100

    Add backward pass for propagating gradients to previous layers, and for optimizer

--- src/layers/dense.py
+++ src/layers/dense.py
@@ -8,6 +8,16 @@ class Dense:
         self.input = None
         self.output = None
 
-    def forward(self, X):
-        self.input = X
+    def forward(self, input):
+        self.input = input
         self.output = np.dot(self.weights, self.input) + self.bias
+
+    def backward(self, output_gradient):
+        input_gradient = np.dot(output_gradient, self.weights.T)
+        weights_gradient = np.dot(self.input.T, output_gradient)
+        bias_gradient = np.sum(output_gradient, axis=0, keepDims=True)
+
+        self.weights_gradient = weights_gradient
+        self.bias_gradient = bias_gradient
+
+        return input_gradient
