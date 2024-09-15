7c8a957c4a3135e905896df14d74ac6178a9e9b9
benamiller <mrbenamiller@gmail.com>
2024-09-15 19:03:18 +0100

    Start of the convolutional

--- src/layers/convolutional.py
+++ src/layers/convolutional.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+
+class Convolutional:
+    def __init__(filters, filter_size, channels):
+        self.weights = np.random.randn(filters, filter_size[0], filter_size[1], channels) * 0.01
+        self.bias = np.random.randn(1, filters)
+        self.input = None
+        self.output = None
+
