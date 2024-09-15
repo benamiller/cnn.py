498e2e1ae8a8b4d5d78144a07fce61491513d28c
benamiller <mrbenamiller@gmail.com>
2024-09-15 22:14:29 +0100

    Have more info so we don't compute the same thing for each forward pass

--- src/layers/convolutional.py
+++ src/layers/convolutional.py
@@ -2,8 +2,17 @@ import numpy as np
 
 
 class Convolutional:
-    def __init__(filters, filter_size, channels):
-        self.weights = np.random.randn(filters, filter_size[0], filter_size[1], channels) * 0.01
+    def __init__(input_shape, filters, filter_size, stride=1, padding=0):
+        self.input_channels, self.input_height, self.input_width = input_shape
+        self.filter_size = filter_size
+        self.filters = filters
+        self.stride = stride
+        self.padding = padding
+
+        self.output_height = ((self.input_height - self.filter_size + 2 * padding) // stride) + 1
+        self.output_width = ((self.input_width - self.filter_size + 2 * padding) // stride) + 1
+
+        self.weights = np.random.randn(filters, filter_size, filter_size, self.input_channels) * 0.01
         self.bias = np.random.randn(1, filters)
         self.input = None
         self.output = None
