93abfd8099786a27d1105e2b854fca90c0fcbee2
benamiller <mrbenamiller@gmail.com>
2024-09-24 14:05:10 +0100

    I don't actually know how this happened. I use neovim btw

--- src/layers/convolutional.py
+++ src/layers/convolutional.py
@@ -31,7 +31,7 @@ class Convolutional:
 
         for h in range(self.output_height):
             for w in range(self.output_width):
-                filter_shadow = self.padded_input[:,:, h*self.stride:h*self.stride+self.filter_size, w*self.stride:w*self.stride:w*self.stride+self.filter_size]
+                filter_shadow = self.padded_input[:,:, h*self.stride:h*self.stride+self.filter_size, w*self.stride:w*self.stride+self.filter_size]
 
                 for f in range(self.filters):
                     self.output[:, f, h, w] = np.sum(filter_shadow * self.weights[f], axis=(1, 2, 3)) + self.bias[f]
@@ -46,7 +46,8 @@ class Convolutional:
 
         for h in range(self.output_height):
             for w in range(self.output_width):
-                filter_shadow = self.input
+                filter_shadow = self.padded_input[:,:, h*self.stride:h*self.stride+self.filter_size, w*self.stride:w*self.stride+self.filter_size] 
+
         return input_gradient * self.weights
 
 
