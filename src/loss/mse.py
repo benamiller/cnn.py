0330d5fd64521bc251fcbeb0753c1a1b4ad36e4d
benamiller <mrbenamiller@gmail.com>
2024-12-08 21:01:40 +0000

    Fixes again

--- src/loss/mse.py
+++ src/loss/mse.py
@@ -5,11 +5,11 @@ class MSE:
         self.preds = preds
         self.labels = labels
 
-    def get_mean_squared_error(self, preds=self.preds, labels=self.labels):
+    def get_mean_squared_error(self):
         
         aggregating_delta = 0
 
-        for i, preds in enumerate(self.predss):
+        for i, preds in enumerate(self.preds):
             aggregating_delta += (preds - self.labels[i])**2
 
         return aggregating_delta
