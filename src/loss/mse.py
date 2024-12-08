f9821d262f743973eb3a11e8672c4b578e946dcb
benamiller <mrbenamiller@gmail.com>
2024-12-08 20:50:22 +0000

    Adds simple mse implementation

--- src/loss/mse.py
+++ src/loss/mse.py
@@ -5,6 +5,13 @@ class MSE:
         self.input = input
         self.labels = labels
 
-    def get_mean_squared_error():
-        for input in self.inputs:
+    def get_mean_squared_error(self, input=self.input, labels=self.labels):
+        
+        aggregating_delta = 0
+
+        for i, input in enumerate(self.inputs):
+            aggregating_delta += (input - self.labels[i])**2
+
+        return aggregating_delta
+
 
