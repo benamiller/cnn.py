4487082a8eeacf304787d19ca7e204efb5b7c745
benamiller <mrbenamiller@gmail.com>
2024-12-08 20:57:15 +0000

    Fixing some embarassing oversights. Has been a minute

--- src/loss/mse.py
+++ src/loss/mse.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 class MSE:
-    def __init__(self, input, labels):
-        self.input = input
+    def __init__(self, preds, labels):
+        self.preds = preds
         self.labels = labels
 
-    def get_mean_squared_error(self, input=self.input, labels=self.labels):
+    def get_mean_squared_error(self, preds=self.preds, labels=self.labels):
         
         aggregating_delta = 0
 
-        for i, input in enumerate(self.inputs):
-            aggregating_delta += (input - self.labels[i])**2
+        for i, preds in enumerate(self.predss):
+            aggregating_delta += (preds - self.labels[i])**2
 
         return aggregating_delta
 
