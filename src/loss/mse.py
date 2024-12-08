58b64be4968c63b2cbb9d1e26c1e30bf091a071b
benamiller <mrbenamiller@gmail.com>
2024-12-08 21:02:36 +0000

    Divide by length for actual MSE

--- src/loss/mse.py
+++ src/loss/mse.py
@@ -12,6 +12,6 @@ class MSE:
         for i, preds in enumerate(self.preds):
             aggregating_delta += (preds - self.labels[i])**2
 
-        return aggregating_delta
+        return aggregating_delta / len(self.preds)
 
 
