059f55478f8dfe75d9feabaf15e31681104e4356
benamiller <mrbenamiller@gmail.com>
2024-12-08 20:47:50 +0000

    Starts MSE class for mse loss

---
+++ src/loss/mse.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class MSE:
+    def __init__(self, input, labels):
+        self.input = input
+        self.labels = labels
+
+    def get_mean_squared_error():
+        for input in self.inputs:
+
